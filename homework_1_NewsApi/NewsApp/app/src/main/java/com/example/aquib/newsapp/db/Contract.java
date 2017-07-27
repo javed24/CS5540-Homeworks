7806717aa3b8e782d45b81d003d1b1d844c4c683
javed24 <aquib_javed@live.com>
2017-07-27 11:47:56 -0700

    added dbhelper and contract for database

---
+++ homework_1_NewsApi/NewsApp/app/src/main/java/com/example/aquib/newsapp/db/Contract.java
@@ -0,0 +1,19 @@
+package com.example.aquib.newsapp.db;
+
+import android.provider.BaseColumns;
+
+/**
+ * Created by aquib on 7/27/17.
+ */
+
+public class Contract {
+    public static class TABLE_ARTICLES implements BaseColumns {
+        public static final String TABLE_NAME="articles";
+        public static final String COLUMN_NAME_AUTHOR="author";
+        public static final String COLUMN_NAME_TITLE="title";
+        public static final String COLUMN_NAME_DESCRIPTION="description";
+        public static final String COLUMN_NAME_NEWS_URL="newsurl";
+        public static final String COLUMN_NAME_IMAGE_URL="imgurl";
+        public static final String COLUMN_NAME_PUBLISHED="published";
+    }
+}
